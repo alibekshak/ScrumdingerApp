331122653f88da6085a376b17875dfed039ca164
alibekshak <alibekshakiro1@gmail.com>
2023-10-18 17:45:16 +0600

    Creating the meeting timer view.

--- Scrumdinger/View/MeetingView.swift
+++ Scrumdinger/View/MeetingView.swift
@@ -11,12 +11,10 @@ struct MeetingView: View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
                 .fill(scrum.theme.mainColor)
+            
             VStack {
-                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
-                                  secondsRemaining: scrumTimer.secondsRemaining,
-                                  theme: scrum.theme)
-                Circle()
-                    .strokeBorder(lineWidth: 22)
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
+                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
