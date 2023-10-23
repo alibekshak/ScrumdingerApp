063a7b27eadec0b23a75fda3e718f432928dec7d
alibekshak <alibekshakiro1@gmail.com>
2023-10-23 12:18:20 +0600

    Transcribing speech to text.

--- Scrumdinger/View/DetailView.swift
+++ Scrumdinger/View/DetailView.swift
@@ -7,40 +7,45 @@ struct DetailView: View {
     @State private var isPresentingEditView = false
     
     var body: some View {
-            List{
-                Section(header: Text("Meeting Info")){
-                    NavigationLink(destination: MeetingView(scrum: $scrum)){
-                        Label("Start Meeting", systemImage: "timer")
-                            .font(.headline)
-                            .foregroundColor(.accentColor)
-                    }
-                    HStack{
-                        Label("Length", systemImage: "clock")
-                        Spacer()
-                        Text("\(scrum.lengthInMinutes) minutes")
-                    }
-                    .accessibilityElement(children: .combine)
-                    HStack{
-                        Label("Theme", systemImage: "paintpalette")
-                        Spacer()
-                        Text(scrum.theme.name)
-                            .padding(4)
-                            .foregroundColor(scrum.theme.accentColor)
-                            .background(scrum.theme.mainColor)
-                            .cornerRadius(4)
-                    }
-                    .accessibilityElement(children: .combine)
+        List{
+            Section(header: Text("Meeting Info")){
+                NavigationLink(destination: MeetingView(scrum: $scrum)){
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
                 }
-                Section(header: Text("Attendees")) {
-                    ForEach(scrum.attendees) { attendee in
-                        Label(attendee.name, systemImage: "person")
-                    }
+                
+                HStack{
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
                 }
-                Section(header: Text("History")){
-                    if scrum.history.isEmpty {
-                        Label("No meetings yet", systemImage: "calendar.badge.exclamationmark")
-                    }
-                    ForEach(scrum.history){ history in
+                .accessibilityElement(children: .combine)
+                
+                HStack{
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+            
+            Section(header: Text("History")){
+                if scrum.history.isEmpty {
+                    Label("No meetings yet", systemImage: "calendar.badge.exclamationmark")
+                }
+                ForEach(scrum.history){ history in
+                    NavigationLink(destination: HistoryView(history: history)) {
                         HStack{
                             Image(systemName: "calendar")
                             Text(history.date, style: .date)
@@ -48,6 +53,7 @@ struct DetailView: View {
                     }
                 }
             }
+        }
         .navigationTitle(scrum.title)
         .toolbar{
             Button("Edit") {
